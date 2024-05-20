bf5b14a38cf318baa3ec6597f6c8ce3f7a1ea7ea
prashant-art <pkatre279@gmail.com>
2024-05-20 14:53:45 +0530

    first commit

---
+++ Empsystem/src/main/java/com/example/Empsystem/EmpsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Empsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpsystemApplication.class, args);
+	}
+
+}
---
+++ Empsystem/src/main/java/com/example/Empsystem/repository/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package com.example.Empsystem.repository;
+
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.Empsystem.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
